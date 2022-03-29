53ebeba1dfcd7aebd00c6963c5fa083e615dadf1
Kirill Morgunov <kirillmorgunov@mail.ru>
2022-03-29 11:57:56 +0300

    собрал проект, добавил шапкÐу

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import  "./css/main.css"
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      Работает!
+    </div>
+  );
+}
+
+export default App;
